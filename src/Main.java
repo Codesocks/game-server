8dbaa8f6ceabde257b6a700eec090af2ed36c1d8
Christian <ickler.christian@gmail.com>
2019-10-18 00:56:36 +0200

    add connect 4 game class with some methods

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
 
     public static void main(String[] args) {
+        int rows = 6;
+        int cols = 7;
+        boolean player1Turn = true;
 
+        Connect4Game myGame = new Connect4Game(rows, cols);
+        myGame.move(3,2);
+        myGame.move(3,1);
+        myGame.move(3,1);
+        myGame.move(1,2);
+        myGame.move(3, 1);
+        System.out.println(myGame.checkWon(2,3,1));
+        myGame.printGame();
     }
 }
