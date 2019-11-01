a4eb6f4ec1c42b507a7b4fdb778b30bdf49d275c
Christian <34313772+Codesocks@users.noreply.github.com>
2019-11-01 11:16:50 +0100

    Delete Game.java

--- src/Game.java
+++
@@ -1,9 +0,0 @@
-public abstract class Game {
-    Player player1 = new Player();
-    Player player2 = new Player();
-    // geht nich weil Field abstract !
-    // Field field = new Field();
-
-    public abstract void move();
-    public abstract void turn();
-}
